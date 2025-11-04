c1b58bdf528e73aecc53adafcc03669c12ecc118
Cyber7711 <sarvareliboyev@gmail.com>
2025-11-04 16:34:46 +0500

    Remove deprecated Mongoose options and fix warnings

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongo ulandi".green);
   } catch (error) {
     console.error("Mongo ulanishida xatolik", error.message, red);
