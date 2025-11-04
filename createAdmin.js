3e2ed6774e27d997fc024b89c7f8df24b26a8512
Cyber7711 <sarvareliboyev@gmail.com>
2025-11-04 17:13:28 +0500

    Remove deprecated Mongoose options and fix warnings

--- createAdmin.js
+++ createAdmin.js
@@ -9,35 +9,33 @@ if (!process.env.MONGO_URI) {
   process.exit(1);
 }
 
-connectDB()(
-  //  (async IIFE)
-  async () => {
-    try {
-      const [username, password] = process.argv.slice(2);
+//  (async IIFE)
+async () => {
+  try {
+    const [username, password] = process.argv.slice(2);
 
-      if (!username || !password) {
-        console.log("Usage: node createAdmin.js <username> <password>");
-        process.exit(1);
-      }
-
-      await connectDB();
-
-      const allAdmins = await Admin.find();
-      if (allAdmins.length > 0) {
-        console.log("⚠️ Eski admin topildi, o‘chirilmoqda...".yellow);
-        await Admin.deleteMany({});
-        console.log("🗑️ Eski admin(lar) o‘chirildi.".red);
-      }
-      const admin = await Admin.create({ username, password });
-      console.log(`✅ Yangi admin yaratildi: ${admin.username}`.green);
-
-      await mongoose.connection.close();
-      console.log("🔌 Ulanish yopildi".yellow);
-
-      process.exit(0);
-    } catch (err) {
-      console.error("Xato:", err.message || err);
+    if (!username || !password) {
+      console.log("Usage: node createAdmin.js <username> <password>");
       process.exit(1);
     }
+
+    await connectDB();
+
+    const allAdmins = await Admin.find();
+    if (allAdmins.length > 0) {
+      console.log("⚠️ Eski admin topildi, o‘chirilmoqda...".yellow);
+      await Admin.deleteMany({});
+      console.log("🗑️ Eski admin(lar) o‘chirildi.".red);
+    }
+    const admin = await Admin.create({ username, password });
+    console.log(`✅ Yangi admin yaratildi: ${admin.username}`.green);
+
+    await mongoose.connection.close();
+    console.log("🔌 Ulanish yopildi".yellow);
+
+    process.exit(0);
+  } catch (err) {
+    console.error("Xato:", err.message || err);
+    process.exit(1);
   }
-)();
+};
