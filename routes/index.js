4f39a692fce4dd1f55fb98f0aa4a8eb4ca5f7b6f
Cyber7711 <sarvareliboyev8@gmail.com>
2026-01-03 18:13:04 +0500

    more secure is continuing

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,15 @@ const applicantRoutes = require("./applicantRoutes");
 const teacherRoutes = require("./teachersRoutes");
 const newsRoutes = require("./newsRoutes");
 const docRoutes = require("./docRoutes");
+const statisticsRoutes = require("./statisticsRoutes");
+const quicklinksRoutes = require("./quicklinksRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/applicant", applicantRoutes);
 router.use("/teachers", teacherRoutes);
 router.use("/news", newsRoutes);
 router.use("/doc", docRoutes);
+router.use("/statistics", statisticsRoutes);
+router.use("/quicklinks", quicklinksRoutes);
 
 module.exports = router;
